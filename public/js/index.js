f13afdb38641c67b9cccf48d19b38cd353fce677
Lotux <beatsbyhellcat@gmail.com>
2023-09-29 13:14:40 -0500

    added verification & bug fixes

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,7 @@ var previewTemp = {
     businessLName: '',
     businessStreetAddress: '',
     businessCity: '',
-    businessState:'',
+    businessState: '',
     businessZip: '',
     businessPhone: '',
     businessEmail: '',
@@ -50,9 +50,9 @@ const addItem = () => {
     productList.push({
         "id": count + 1,
         "product_title": "Product Title",
-        "product_desc": "Product Description",
-        "product_price": 0,
-        "product_quantity": 1
+        "product_desc": "",
+        "product_price": "",
+        "product_quantity": ""
     })
     init()
 }
@@ -85,18 +85,18 @@ const saveInput = (elem, id) => {
     if ($(elem).attr('data-desc')) {
 
 
-  
-            productList.find(x => x.id === id).product_desc = soup;
-        
+
+        productList.find(x => x.id === id).product_desc = soup;
+
     } else if ($(elem).attr('data-price')) {
 
 
-            productList.find(x => x.id === id).product_price = parseInt(soup);
+        productList.find(x => x.id === id).product_price = parseInt(soup);
 
     } else if ($(elem).attr('data-quantity')) {
 
-            productList.find(x => x.id === id).product_quantity = parseInt(soup);
-        
+        productList.find(x => x.id === id).product_quantity = parseInt(soup);
+
     }
 
 }
@@ -137,7 +137,7 @@ const init = () => {
         </button>
       <div class="overflow-hidden">
         <div class="input-group">
-          <input onblur="saveInput(this,${productList[i].id})" name="desc" value="${productList[i].product_desc}" data-desc="Product Description" type="text" class="form-control product-detail border-0 w-50" data-bs-toggle="tooltip" data-bs-title="Desc."
+          <input onblur="saveInput(this,${productList[i].id})" name="desc" value="${productList[i].product_desc}" data-desc="Product Description" type="text" class="form-control product-detail border-0 w-50" data-bs-toggle="tooltip" data-bs-title="Product Description"
             placeholder="Desc." aria-label="Desc.">
           <input onblur="saveInput(this,${productList[i].id})" name="price" value="${productList[i].product_price}"  data-price="0" type="number" class="form-control product-detail border-0 w-25" data-bs-toggle="tooltip" data-bs-title="Price"
             placeholder="Price" aria-label="Price">
@@ -163,34 +163,249 @@ class="bi bi-plus-circle" viewBox="0 0 16 16">
 init();
 
 
-const preview = () =>{
-    previewTemp.invoiceNumber = $('#invoiceNum').val();
-    previewTemp.invoiceDate = $('#invoiceDate').val();
-    previewTemp.invoiceDue = $('#invoiceDueDate').val();
-    previewTemp.businessName = $('#businessName').val();
-    previewTemp.businessFName = $('#businessFName').val();
-    previewTemp.businessLName = $('#businessLName').val();
-    previewTemp.businessStreetAddress = $('#businessStreetAddress').val();
-    previewTemp.businessCity = $('#businessCity').val();
-    previewTemp.businessState = $('#businessState').val();
-    previewTemp.businessZip = $('#businessZip').val();
-    previewTemp.businessPhone = $('#businessNumber').val();
-    previewTemp.businessEmail = $('#businessEmail').val();
-    previewTemp.clientFName = $('#clientFName').val();
-    previewTemp.clientLName = $('#clientLName').val();
-    previewTemp.clientStreetAddress = $('#clientStreetAddress').val();
-    previewTemp.clientCity = $('#clientCity').val();
-    previewTemp.clientState = $('#clientState').val();
-    previewTemp.clientZip = $('#clientZip').val();
-    previewTemp.clientNumber = $('#clientNumber').val();
-    previewTemp.clientEmail = $('#clientEmail').val();
-    previewTemp.paymentNotes = $('#paymentNotes').val();
-    previewTemp.tax = parseFloat($('#tax').val());
-    previewTemp.discount = parseFloat($('#discount').val());
+const preview = () => {
+    if ($('#invoiceNum').val().length <= 0) {
+        $('#invoiceNum').addClass('text-danger border-danger');
+        $('#invoiceNum').removeClass('text-success border-success');
+        $('#invoiceNum').focus();
 
-    localStorage.setItem("Preview", JSON.stringify(previewTemp));
-    localStorage.setItem("Products", JSON.stringify(productList));
-    window.open("./invoice.html", '_blank').focus();
+    } else {
+        $('#invoiceNum').removeClass('text-danger borer-danger');
+        $('#invoiceNum').addClass('text-success border-success');
+    }
+
+    if ($('#invoiceDate').val().length <= 0 && $('#invoiceDueDate').val().length <= 0) {
+        $('#invoiceDate').addClass('text-danger border-danger');
+        $('#invoiceDate').removeClass('text-success border-success');
+        $('#invoiceDueDate').addClass('text-danger border-danger');
+        $('#invoiceDueDate').removeClass('text-success border-success');
+
+    } else {
+        $('#invoiceDate').removeClass('text-danger borer-danger');
+        $('#invoiceDate').addClass('text-success border-success');
+        $('#invoiceDueDate').removeClass('text-danger borer-danger');
+        $('#invoiceDueDate').addClass('text-success border-success');
+    }
+
+    if ($('#businessName').val().length <= 0) {
+        $('#businessName').addClass('text-danger border-danger');
+        $('#businessName').removeClass('text-success border-success');
+    } else {
+        $('#businessName').removeClass('text-danger borer-danger');
+        $('#businessName').addClass('text-success border-success');
+    }
+
+    if ($('#businessFName').val().length <= 0) {
+        $('#businessFName').addClass('text-danger border-danger');
+        $('#businessFName').removeClass('text-success border-success');
+    } else {
+        $('#businessFName').removeClass('text-danger borer-danger');
+        $('#businessFName').addClass('text-success border-success');
+    }
+
+    if ($('#businessLName').val().length <= 0) {
+        $('#businessLName').addClass('text-danger border-danger');
+        $('#businessLName').removeClass('text-success border-success');
+    } else {
+        $('#businessLName').removeClass('text-danger borer-danger');
+        $('#businessLName').addClass('text-success border-success');
+    }
+
+    if ($('#businessStreetAddress').val().length <= 0) {
+        $('#businessStreetAddress').addClass('text-danger border-danger');
+        $('#businessStreetAddress').removeClass('text-success border-success');
+    } else {
+        $('#businessStreetAddress').removeClass('text-danger borer-danger');
+        $('#businessStreetAddress').addClass('text-success border-success');
+    }
+
+    if ($('#businessCity').val().length <= 0) {
+        $('#businessCity').addClass('text-danger border-danger');
+        $('#businessCity').removeClass('text-success border-success');
+    } else {
+        $('#businessCity').removeClass('text-danger borer-danger');
+        $('#businessCity').addClass('text-success border-success');
+    }
+
+    if ($('#businessState').val().length <= 0) {
+        $('#businessState').addClass('text-danger border-danger');
+        $('#businessState').removeClass('text-success border-success');
+    } else {
+        $('#businessState').removeClass('text-danger borer-danger');
+        $('#businessState').addClass('text-success border-success');
+    }
+
+    if ($('#businessZip').val().length <= 0) {
+        $('#businessZip').addClass('text-danger border-danger');
+        $('#businessZip').removeClass('text-success border-success');
+    } else {
+        $('#businessZip').removeClass('text-danger borer-danger');
+        $('#businessZip').addClass('text-success border-success');
+    }
+
+    if ($('#businessNumber').val().length <= 0) {
+        $('#businessNumber').addClass('text-danger border-danger');
+        $('#businessNumber').removeClass('text-success border-success');
+    } else {
+        $('#businessNumber').removeClass('text-danger borer-danger');
+        $('#businessNumber').addClass('text-success border-success');
+    }
+
+    if ($('#businessEmail').val().length <= 0) {
+        $('#businessEmail').addClass('text-danger border-danger');
+        $('#businessEmail').removeClass('text-success border-success');
+    } else {
+        $('#businessEmail').removeClass('text-danger borer-danger');
+        $('#businessEmail').addClass('text-success border-success');
+    }
+
+    if ($('#clientFName').val().length <= 0) {
+        $('#clientFName').addClass('text-danger border-danger');
+        $('#clientFName').removeClass('text-success border-success');
+    } else {
+        $('#clientFName').removeClass('text-danger borer-danger');
+        $('#clientFName').addClass('text-success border-success');
+    }
+
+    if ($('#clientLName').val().length <= 0) {
+        $('#clientLName').addClass('text-danger border-danger');
+        $('#clientLName').removeClass('text-success border-success');
+    } else {
+        $('#clientLName').removeClass('text-danger borer-danger');
+        $('#clientLName').addClass('text-success border-success');
+    }
+
+    if ($('#clientStreetAddress').val().length <= 0) {
+        $('#clientStreetAddress').addClass('text-danger border-danger');
+        $('#clientStreetAddress').removeClass('text-success border-success');
+    } else {
+        $('#clientStreetAddress').removeClass('text-danger borer-danger');
+        $('#clientStreetAddress').addClass('text-success border-success');
+    }
+
+    if ($('#clientCity').val().length <= 0) {
+        $('#clientCity').addClass('text-danger border-danger');
+        $('#clientCity').removeClass('text-success border-success');
+    } else {
+        $('#clientCity').removeClass('text-danger borer-danger');
+        $('#clientCity').addClass('text-success border-success');
+    }
+
+    if ($('#clientState').val().length <= 0) {
+        $('#clientState').addClass('text-danger border-danger');
+        $('#clientState').removeClass('text-success border-success');
+    } else {
+        $('#clientState').removeClass('text-danger borer-danger');
+        $('#clientState').addClass('text-success border-success');
+
+    }
+
+    if ($('#clientZip').val().length <= 0) {
+        $('#clientZip').addClass('text-danger border-danger');
+        $('#clientZip').removeClass('text-success border-success');
+    } else {
+        $('#clientZip').removeClass('text-danger borer-danger');
+        $('#clientZip').addClass('text-success border-success');
+
+    }
+
+    if ($('#clientNumber').val().length <= 0) {
+        $('#clientNumber').addClass('text-danger border-danger');
+        $('#clientNumber').removeClass('text-success border-success');
+    } else {
+        $('#clientNumber').removeClass('text-danger borer-danger');
+        $('#clientNumber').addClass('text-success border-success');
+
+    }
+
+    if ($('#clientEmail').val().length <= 0) {
+        $('#clientEmail').addClass('text-danger border-danger');
+        $('#clientEmail').removeClass('text-success border-success');
+    } else {
+        $('#clientEmail').removeClass('text-danger borer-danger');
+        $('#clientEmail').addClass('text-success border-success');
+
+    }
+
+    if ($('#clientEmail').hasClass('text-danger') ||
+        $('#clientNumber').hasClass('text-danger') ||
+        $('#clientZip').hasClass('text-danger') ||
+        $('#clientState').hasClass('text-danger') ||
+        $('#clientCity').hasClass('text-danger') ||
+        $('#clientStreetAddress').hasClass('text-danger') ||
+        $('#clientLName').hasClass('text-danger') ||
+        $('#clientFName').hasClass('text-danger')) {
+        $('#billDetailHeader').addClass('bg-danger-subtle text-danger border-danger');
+    } else {
+        $('#billDetailHeader').removeClass('bg-danger-subtle text-danger border-danger');
+    }
+
+    if (
+        $('#invoiceDate').hasClass('text-danger') ||
+        $('#businessEmail').hasClass('text-danger') ||
+        $('#businessNumber').hasClass('text-danger') ||
+        $('#businessZip').hasClass('text-danger') ||
+        $('#businessState').hasClass('text-danger') ||
+        $('#businessCity').hasClass('text-danger') ||
+        $('#businessStreetAddress').hasClass('text-danger') ||
+        $('#businessLName').hasClass('text-danger') ||
+        $('#businessFName').hasClass('text-danger')) {
+        $('#businessDetailHeader').addClass('bg-danger-subtle text-danger border-danger');
+    } else {
+        $('#businessDetailHeader').removeClass('bg-danger-subtle text-danger  border-danger');
+
+
+    }
+
+    for (var t = 0; t < productList.length; t++){
+        if (productList[t].product_desc.length <= 0 || productList[t].product_quantity.length <= 0
+            || productList[t].product_price.length <= 0){
+    
+                $('li').attr('value', [t]).addClass('list-group-item-danger')
+               $('#orderDetails').addClass('bg-danger-subtle text-danger border-danger');
+               return
+            }else{
+                $('#orderDetails').removeClass('bg-danger-subtle text-danger  border-danger');
+            }
+    }
+
+    if ($('#invoiceNum').hasClass('text-danger') ||
+        $('#invoiceDueDate').hasClass('text-danger') ||
+        $('#businessDetailHeader').hasClass('text-danger') ||
+        $('#billDetailHeader').hasClass('text-danger')) {
+            return;
+
+    } else {
+        previewTemp.clientEmail = $('#clientEmail').val();
+        previewTemp.clientNumber = $('#clientNumber').val();
+        previewTemp.clientZip = $('#clientZip').val();
+        previewTemp.clientState = $('#clientState').val();
+        previewTemp.clientCity = $('#clientCity').val();
+        previewTemp.clientStreetAddress = $('#clientStreetAddress').val();
+        previewTemp.clientLName = $('#clientLName').val();
+        previewTemp.clientFName = $('#clientFName').val();
+        previewTemp.businessEmail = $('#businessEmail').val();
+        previewTemp.businessPhone = $('#businessNumber').val();
+        previewTemp.businessZip = $('#businessZip').val();
+        previewTemp.businessState = $('#businessState').val();
+        previewTemp.businessCity = $('#businessCity').val();
+        previewTemp.businessStreetAddress = $('#businessStreetAddress').val();
+        previewTemp.businessLName = $('#businessLName').val();
+        previewTemp.businessFName = $('#businessFName').val();
+        previewTemp.businessName = $('#businessName').val();
+        previewTemp.invoiceDate = $('#invoiceDate').val();
+        previewTemp.invoiceDue = $('#invoiceDueDate').val();
+        previewTemp.invoiceNumber = $('#invoiceNum').val();
+        previewTemp.paymentNotes = $('#paymentNotes').val();
+        previewTemp.tax = parseFloat($('#tax').val());
+        previewTemp.discount = parseFloat($('#discount').val());
+        localStorage.setItem("Preview", JSON.stringify(previewTemp));
+        localStorage.setItem("Products", JSON.stringify(productList));
+        window.open("./invoice.html", '_blank').focus();
+    }
+
+
+
 
 }
 const invoiceNumGen = (min = 0, max = 500000) => {
@@ -205,6 +420,8 @@ const reset = () => {
     $('.product-card').removeClass('active');
 
 }
+
+
 
 
 
